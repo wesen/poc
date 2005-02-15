ca75a82044db4616550c563c781f63a262b96702
manuel <manuel@bf09bf55-38ec-0310-9d3f-cd34fc72d291>
2005-02-15 18:02:06 +0000

    jetzt auch mit encoding (immer noch nicht getestet)
    git-svn-id: svn+ssh://bl0rg.net/srv/svn/poc/trunk@177 bf09bf55-38ec-0310-9d3f-cd34fc72d291

--- libfec.c
+++ libfec.c
@@ -7,9 +7,11 @@
 #include <unistd.h>
 #include <assert.h>
 
-#include "libfec.h"
+#include "fec.h"
 #include "fec-group.h"
 #include "fec-pkt.h"
+
+#include "libfec.h"
 
 static aq_t fec_aq;
 static int initialized = 0;
@@ -46,6 +48,8 @@ void libfec_add_pkt(fec_decode_t *group,
                  unsigned char *data) {
   assert(group != NULL);
   assert(data != NULL);
+  assert(len <= group->fec_len);
+  assert(pkt_seq <= group->fec_n);
   
   fec_pkt_t pkt;
   fec_pkt_init(&pkt);
@@ -86,4 +90,122 @@ void libfec_delete_group(fec_decode_t *group) {
   assert(group != NULL);
   fec_group_destroy(group);
   free(group);
+}
+
+struct fec_encode_s {
+  unsigned char fec_k;
+  unsigned char fec_n;
+  fec_t *fec;
+
+  unsigned char *adus;
+  unsigned int  *lengths;
+  unsigned char **adu_ptrs;
+  
+  unsigned char count;
+  unsigned int  max_length;
+};
+
+fec_encode_t *libfec_new_encode(unsigned char fec_k,
+                                unsigned char fec_n) {
+  if (fec_n <= fec_k)
+    return NULL;
+  
+  fec_encode_t *encode = malloc(sizeof(fec_encode_t));
+  if (encode == NULL)
+    return NULL;
+
+  encode->fec_n        = fec_n;
+  encode->fec_k        = fec_k;
+  encode->adus         = NULL;
+  encode->adu_ptrs     = NULL;
+  encode->lengths      = NULL;
+  encode->count        = 0;
+  encode->max_length   = 0;
+  
+  encode->fec = fec_new(encode->fec_k, encode->fec_n);
+  if (encode->fec == NULL)
+    goto exit;
+  
+  encode->adus = malloc(FEC_PKT_PAYLOAD_SIZE * fec_k);
+  if (encode->adus == NULL)
+    goto exit;
+  memset(encode->adus, 0, FEC_PKT_PAYLOAD_SIZE * fec_k);
+
+  encode->adu_ptrs = malloc(fec_k * sizeof(unsigned char *));
+  if (encode->adu_ptrs == NULL)
+    goto exit;
+  encode->lengths = malloc(sizeof(unsigned int) * fec_k);
+  if (encode->lengths == NULL)
+    goto exit;
+  int i;
+  for (i = 0; i < encode->fec_k; i++) {
+    encode->adu_ptrs[i] = encode->adus + i * FEC_PKT_PAYLOAD_SIZE;
+    encode->lengths[i] = 0;
+  }
+
+  return encode;
+
+exit:
+  libfec_delete_encode(encode);
+  return NULL;
+}
+
+void libfec_delete_encode(fec_encode_t *encode) {
+  assert(encode != NULL);
+  if (encode->adus)
+    free(encode->adus);
+  if (encode->lengths)
+    free(encode->lengths);
+  if (encode->adu_ptrs)
+    free(encode->adu_ptrs);
+  if (encode->fec)
+    fec_free(encode->fec);
+  free(encode);
+}
+
+int libfec_add_adu(fec_encode_t *encode,
+                   unsigned long len,
+                   unsigned char *data) {
+  assert(encode != NULL);
+  assert(encode->lengths != NULL);
+  assert(data != NULL);
+
+  if ((encode->adus == NULL) ||
+      (encode->count >= encode->fec_k))
+    return 0;
+
+  encode->lengths[encode->count] = len;
+  memcpy(encode->adus + FEC_PKT_PAYLOAD_SIZE * encode->count,
+         data, len);
+  encode->count++;
+  if (encode->max_length < len)
+    encode->max_length = len;
+
+  return 1;
+}
+
+unsigned int libfec_max_length(fec_encode_t *encode) {
+  return encode->max_length;
+}
+
+unsigned int libfec_encode(fec_encode_t *encode,
+                           unsigned char *dst,
+                           unsigned int idx,
+                           unsigned int len) {
+  assert(encode != NULL);
+  if (encode->count != encode->fec_k)
+    return 0;
+  if (encode->max_length > len)
+    return 0;
+  
+  assert(encode->adu_ptrs != NULL);
+  assert(encode->fec != NULL);
+  assert(dst != NULL);
+  fec_encode(encode->fec, encode->adu_ptrs,
+             dst, idx, len);
+
+  if (idx < encode->fec_k)
+    return encode->lengths[idx];
+  else
+    return encode->max_length;
 }
--- libfec.h
+++ libfec.h
@@ -4,24 +4,68 @@
 struct fec_group_s;
 typedef struct fec_group_s fec_decode_t;
 
+struct fec_encode_s;
+typedef struct fec_encode_s fec_encode_t;
+
 void libfec_init(void);
 void libfec_close(void);
 void libfec_reset(void);
 
 /* fec_len is the maximum packet size in the group */
 fec_decode_t *libfec_new_group(unsigned char fec_k,
-                              unsigned char fec_n,
-                              unsigned long fec_len);
+                               unsigned char fec_n,
+                               unsigned long fec_len);
 void libfec_delete_group(fec_decode_t *group);
 
-/* Decoding routines */
+fec_encode_t *libfec_new_encode(unsigned char fec_k,
+                                unsigned char fec_n);
+void libfec_delete_encode(fec_encode_t *encode);
+
+/****** Decoding routines ******/
+
+/*
+ * Add the packet with sequence number (0 <= pkt_seq < fec_n) to the
+ * FEC group.
+ */
 void libfec_add_pkt(fec_decode_t *group,
                     unsigned char pkt_seq,
                     unsigned long len,
                     unsigned char *data);
-/* return 1 on success, 0 on error */
+/*
+ * Decode the FEC packets in the group, and write the resulting MP3
+ * frames on stdout.
+ *
+ * Return 1 on success, 0 on error
+ */
 int libfec_decode(fec_decode_t *group);
 
-/* Encoding routines */
+/****** Encoding routines ******/
+
+/*
+ * Add an ADU (in sequence) into the encoding buffer.
+ *
+ * Return 0 on error, 1 on success.
+ */
+int libfec_add_adu(fec_encode_t *encode,
+                   unsigned long len,
+                   unsigned char *data);
+/*
+ * after fec_k adus have been added, encode the packet with index IDX
+ * into dst, with maximal length len.
+ *
+ * If idx < fec_k, then the original ADUs is copied.
+ *
+ * Returns 0 on error, length of the packet on success.
+ */
+unsigned int libfec_encode(fec_encode_t *encode,
+                           unsigned char *dst,
+                           unsigned int idx,
+                           unsigned int len);
+
+/*
+ * Return the length of the biggest ADU in the group (and thus the
+ * length of the FEC pkts with index >= fec_k.
+ */
+unsigned int libfec_max_length(fec_encode_t *encode);
 
 #endif /* LIBFEC_H__ */
