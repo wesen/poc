5b5b7677f5b2ac83e039b05adb35e5650b977a49
manuel <manuel@bf09bf55-38ec-0310-9d3f-cd34fc72d291>
2005-03-10 09:41:03 +0000

    fuer NONBLOCK IO natuerlich kein unix_read(), da war read() schon richtig
    git-svn-id: svn+ssh://bl0rg.net/srv/svn/poc/trunk@270 bf09bf55-38ec-0310-9d3f-cd34fc72d291

--- http.c
+++ http.c
@@ -303,8 +303,8 @@ static int http_handle_client(http_server_t *server,
   int  tmp;
   
   /* Read header data. */
-  tmp = unix_read(client->fd, client->buf + client->len, 
-		  HTTP_MAX_HDR_LEN - client->len - 5);
+  tmp = read(client->fd, client->buf + client->len, 
+	     HTTP_MAX_HDR_LEN - client->len - 5);
   
   if (tmp <= 0)
     return -1;
